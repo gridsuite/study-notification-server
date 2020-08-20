68858c05799e1d54cff944103c8f80b48ded43b8
HOMER Etienne <etiennehomer@gmail.com>
2020-08-20 16:59:14 +0200

    Add test with specific chars in study name
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/test/java/org/gridsuite/notification/server/NotificationWebSocketHandlerTest.java
+++ src/test/java/org/gridsuite/notification/server/NotificationWebSocketHandlerTest.java
@@ -58,7 +58,7 @@ public class NotificationWebSocketHandlerTest {
 
         ws = Mockito.mock(WebSocketSession.class);
         handshakeinfo = Mockito.mock(HandshakeInfo.class);
-        uriComponentBuilder = UriComponentsBuilder.fromUriString("http://localhost:1234/notify");
+        uriComponentBuilder = UriComponentsBuilder.fromUriString("http://localhost:1234/notify?studyName=foo/bar?foo");
 
         when(handshakeinfo.getUri()).thenReturn(uriComponentBuilder.build().toUri());
         when(ws.getHandshakeInfo()).thenReturn(handshakeinfo);
