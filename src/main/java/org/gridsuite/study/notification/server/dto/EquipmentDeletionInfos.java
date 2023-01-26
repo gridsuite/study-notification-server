4dafdb09fa1af1e6f66f3ac79e5e2820edd6131f
Ayoub LABIDI <117761394+ayolab@users.noreply.github.com>
2023-01-26 14:55:30 +0100

    Regroup equipmentDeletion notifications (#52)
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>

---
+++ src/main/java/org/gridsuite/study/notification/server/dto/EquipmentDeletionInfos.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.gridsuite.study.notification.server.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EquipmentDeletionInfos {
+    String equipmentId;
+    String equipmentType;
+}
+
