fc58b96ec7f282c4a19d9b08d50c9616251770de
klesaulnier <42617371+archendel@users.noreply.github.com>
2023-08-11 10:53:37 +0200

    feat: reference_node_uuid is now an accepted header (#61)
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/org/gridsuite/study/notification/server/NotificationWebSocketHandler.java
+++ src/main/java/org/gridsuite/study/notification/server/NotificationWebSocketHandler.java
@@ -71,6 +71,7 @@ public class NotificationWebSocketHandler implements WebSocketHandler {
     static final String HEADER_MOVED_NODE = "movedNode";
     static final String HEADER_REMOVE_CHILDREN = "removeChildren";
     static final String HEADER_INSERT_MODE = "insertMode";
+    static final String HEADER_REFERENCE_NODE_UUID = "referenceNodeUuid";
 
     private ObjectMapper jacksonObjectMapper;
 
@@ -133,6 +134,7 @@ public class NotificationWebSocketHandler implements WebSocketHandler {
         passHeader(messageHeader, resHeader, HEADER_NEW_NODE);
         passHeader(messageHeader, resHeader, HEADER_MOVED_NODE);
         passHeader(messageHeader, resHeader, HEADER_USER_ID); // to filter the display of error messages in the front end
+        passHeader(messageHeader, resHeader, HEADER_REFERENCE_NODE_UUID);
 
         return resHeader;
     }
