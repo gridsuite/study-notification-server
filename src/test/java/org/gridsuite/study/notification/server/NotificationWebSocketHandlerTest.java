4a53dc647ab55fa713dab66cae5fd4d883b9fef2
Franck LECUYER <franck.lecuyer@rte-france.com>
2024-04-15 16:17:23 +0200

    Remove no more used headers
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/study/notification/server/NotificationWebSocketHandlerTest.java
+++ src/test/java/org/gridsuite/study/notification/server/NotificationWebSocketHandlerTest.java
@@ -222,8 +222,6 @@ public class NotificationWebSocketHandlerTest {
         passHeaderRef(messageHeader, resHeader, HEADER_PARENT_NODE);
         passHeaderRef(messageHeader, resHeader, HEADER_INSERT_MODE);
         passHeaderRef(messageHeader, resHeader, HEADER_INDEXATION_STATUS);
-        passHeaderRef(messageHeader, resHeader, HEADER_REACTIVE_SLACKS_OVER_THRESHOLD);
-        passHeaderRef(messageHeader, resHeader, HEADER_REACTIVE_SLACKS_THRESHOLD_VALUE);
 
         resHeader.remove(HEADER_TIMESTAMP);
 
