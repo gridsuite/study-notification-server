36a3e129d3a0fe799b99298748ff44423d20bac1
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2024-06-06 11:34:56 +0200

    Fix concurrency issue in test

--- src/test/java/org/gridsuite/study/notification/server/NotificationWebSocketIT.java
+++ src/test/java/org/gridsuite/study/notification/server/NotificationWebSocketIT.java
@@ -22,6 +22,8 @@ import reactor.core.publisher.Mono;
 
 import java.net.URI;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 
 import static org.gridsuite.study.notification.server.NotificationWebSocketHandler.*;
 import static org.junit.Assert.assertEquals;
@@ -60,10 +62,15 @@ public class NotificationWebSocketIT {
         String user = "test";
         httpHeaders1.add(HEADER_USER_ID, user);
         Map<String, Double> exp = Map.of(user, 2d);
-        Mono<Void> connection1 = client1.execute(getUrl("/notify"), httpHeaders1, ws -> Mono.fromRunnable(() -> testMeterMap(exp)));
-        Mono<Void> connection2 = client1.execute(getUrl("/notify"), httpHeaders1, ws -> Mono.fromRunnable(() -> testMeterMap(exp)));
+        CountDownLatch connectionLatch = new CountDownLatch(2);
+        CountDownLatch assertLatch = new CountDownLatch(1);
 
+        Mono<Void> connection1 = client1.execute(getUrl("/notify"), httpHeaders1, ws -> Mono.fromRunnable(() -> handleLatches(connectionLatch, assertLatch)));
+        Mono<Void> connection2 = client1.execute(getUrl("/notify"), httpHeaders1, ws -> Mono.fromRunnable(() -> handleLatches(connectionLatch, assertLatch)));
+
+        CompletableFuture<Void> evaluationFuture = evaluateAssert(connectionLatch, exp, assertLatch);
         Mono.zip(connection1, connection2).block();
+        evaluationFuture.join(); // Throw assertion errors
     }
 
     @Test
@@ -75,16 +82,42 @@ public class NotificationWebSocketIT {
         httpHeaders1.add(HEADER_USER_ID, user1);
         String user2 = "test1";
         Map<String, Double> exp = Map.of(user1, 2d, user2, 1d);
-        Mono<Void> connection1 = client1.execute(getUrl("/notify"), httpHeaders1, ws -> Mono.fromRunnable(() -> testMeterMap(exp)));
-        Mono<Void> connection2 = client1.execute(getUrl("/notify"), httpHeaders1, ws -> Mono.fromRunnable(() -> testMeterMap(exp)));
+        CountDownLatch connectionLatch = new CountDownLatch(3);
+        CountDownLatch assertLatch = new CountDownLatch(1);
+        Mono<Void> connection1 = client1.execute(getUrl("/notify"), httpHeaders1, ws -> Mono.fromRunnable(() -> handleLatches(connectionLatch, assertLatch)));
+        Mono<Void> connection2 = client1.execute(getUrl("/notify"), httpHeaders1, ws -> Mono.fromRunnable(() -> handleLatches(connectionLatch, assertLatch)));
 
         // Second WebSocketClient for connections related to 'test1' user
         WebSocketClient client2 = new StandardWebSocketClient();
         HttpHeaders httpHeaders2 = new HttpHeaders();
         httpHeaders2.add(HEADER_USER_ID, user2);
-        Mono<Void> connection3 = client2.execute(getUrl("/notify"), httpHeaders2, ws -> Mono.fromRunnable(() -> testMeterMap(exp)));
+        Mono<Void> connection3 = client2.execute(getUrl("/notify"), httpHeaders2, ws -> Mono.fromRunnable(() -> handleLatches(connectionLatch, assertLatch)));
 
+        CompletableFuture<Void> evaluationFuture = evaluateAssert(connectionLatch, exp, assertLatch);
         Mono.zip(connection1, connection2, connection3).block();
+        evaluationFuture.join(); // Throw assertion errors
+    }
+
+    private void handleLatches(CountDownLatch connectionLatch, CountDownLatch assertLatch) {
+        try {
+            connectionLatch.countDown();
+            assertLatch.await(); // Wait for assertion to be evaluated before closing the connection
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private CompletableFuture<Void> evaluateAssert(CountDownLatch connectionLatch, Map<String, Double> exp, CountDownLatch assertLatch) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                connectionLatch.await();  // Wait for connections to be established
+                testMeterMap(exp);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                assertLatch.countDown(); // Close connections if there is an assertion error
+            }
+        });
     }
 
     private void testMeterMap(Map<String, Double> userMap) {
