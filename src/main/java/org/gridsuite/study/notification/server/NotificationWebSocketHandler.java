2dd1f8d6854f7428c796a450b2360d6c14c83754
Thang PHAM <117309322+thangqp@users.noreply.github.com>
2025-06-23 09:29:21 +0200

    Computation debug mode (#102)
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/main/java/org/gridsuite/study/notification/server/NotificationWebSocketHandler.java
+++ src/main/java/org/gridsuite/study/notification/server/NotificationWebSocketHandler.java
@@ -79,6 +79,7 @@ public class NotificationWebSocketHandler implements WebSocketHandler {
     static final String HEADER_REFERENCE_NODE_UUID = "referenceNodeUuid";
     static final String HEADER_INDEXATION_STATUS = "indexation_status";
     static final String HEADER_COMPUTATION_TYPE = "computationType";
+    static final String HEADER_RESULT_UUID = "resultUuid";
 
     static final String USERS_METER_NAME = "app.users";
     static final String USER_TAG = "user";
@@ -154,6 +155,7 @@ public class NotificationWebSocketHandler implements WebSocketHandler {
         passHeader(messageHeader, resHeader, HEADER_REFERENCE_NODE_UUID);
         passHeader(messageHeader, resHeader, HEADER_INDEXATION_STATUS);
         passHeader(messageHeader, resHeader, HEADER_COMPUTATION_TYPE);
+        passHeader(messageHeader, resHeader, HEADER_RESULT_UUID);
 
         return resHeader;
     }
