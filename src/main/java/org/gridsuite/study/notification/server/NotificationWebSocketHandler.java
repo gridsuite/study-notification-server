d31bc6f67e220e7e31fbc0caa12eb3b291a322ec
Rehili Ghazwa <ghazwarhili@gmail.com>
2025-09-18 17:48:51 +0200

    export network notification header

--- src/main/java/org/gridsuite/study/notification/server/NotificationWebSocketHandler.java
+++ src/main/java/org/gridsuite/study/notification/server/NotificationWebSocketHandler.java
@@ -80,6 +80,9 @@ public class NotificationWebSocketHandler implements WebSocketHandler {
     static final String HEADER_INDEXATION_STATUS = "indexation_status";
     static final String HEADER_COMPUTATION_TYPE = "computationType";
     static final String HEADER_RESULT_UUID = "resultUuid";
+    static final String HEADER_FILE_NAME = "fileName";
+    static final String HEADER_FORMAT = "format";
+    static final String HEADER_EXPORT_UUID = "exportUuid";
 
     static final String USERS_METER_NAME = "app.users";
     static final String USER_TAG = "user";
@@ -156,6 +159,9 @@ public class NotificationWebSocketHandler implements WebSocketHandler {
         passHeader(messageHeader, resHeader, HEADER_INDEXATION_STATUS);
         passHeader(messageHeader, resHeader, HEADER_COMPUTATION_TYPE);
         passHeader(messageHeader, resHeader, HEADER_RESULT_UUID);
+        passHeader(messageHeader, resHeader, HEADER_FILE_NAME);
+        passHeader(messageHeader, resHeader, HEADER_FORMAT);
+        passHeader(messageHeader, resHeader, HEADER_EXPORT_UUID);
 
         return resHeader;
     }
